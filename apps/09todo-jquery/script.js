8d801718ed326a9e6e0c72e5f6313064da75dfb5
Rampaggio <rampyatx@gmail.com>
2016-04-13 19:25:07 -0500

    meh

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,16 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var todoList = $(this).find('#todo').val();
+        console.log(todoList);
+        $('#todo-list').append('<li>' 
+                               + '<input type="checkbox">'
+                               + '<label>'
+                               + todoList
+                               + '</label>'
+                               + '</li>');
+    })
+    $('ul').sortable();
 });
